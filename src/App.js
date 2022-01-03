fdca80ffb7d9f4add52f4a7a84a3ec07c165f393
DimoRadonjic <dimo.radonjic2112@gmail.com>
2022-01-04 00:15:19 +0100

    Making Card Choices

--- src/App.js
+++ src/App.js
@@ -11,17 +11,27 @@ const cardImages = [
   { src: '/img/sword-1.png' },
 ];
 
-class App1 extends React.Component {
+class App extends React.Component {
   state = {
     cards: [],
     turn: 0,
+    choiceOne: null,
+    choiceTwo: null,
   };
+
   shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
     this.setState({ cards: shuffledCards, turn: 0 });
   };
+
+  handleChoice = (card) => {
+    this.state.choiceOne
+      ? this.setState({ choiceTwo: card })
+      : this.setState({ choiceOne: card });
+  };
+
   render() {
     console.log(this.state.cards);
     return (
@@ -30,7 +40,13 @@ class App1 extends React.Component {
         <button onClick={this.shuffleCards}>New Game</button>
         <div className='card-grid'>
           {this.state.cards.map((card) => {
-            return <SingleCard card={card} key={card.id} />;
+            return (
+              <SingleCard
+                card={card}
+                key={card.id}
+                handleChoice={this.handleChoice}
+              />
+            );
           })}
         </div>
       </div>
@@ -38,4 +54,4 @@ class App1 extends React.Component {
   }
 }
 
-export default App1;
+export default App;
