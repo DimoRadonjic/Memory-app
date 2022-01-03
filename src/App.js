c4fe5ba983551bd0bc75f3978b8e5c7c73748c6b
DimoRadonjic <dimo.radonjic2112@gmail.com>
2022-01-03 23:04:35 +0100

    Mainly adding css and changing app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './App.css';
 
 function App() {
-  return <div className='App'>New Project</div>;
+  return (
+    <div className='App'>
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
 }
 
 export default App;
